fa23c4a0c45455babb4e800739a490b24a2ddbbb
Bailian4ik <bailian4ik@mail.ru>
2017-08-28 23:04:16 +0300

    Add files via upload

--- Instailer.lua
+++ Instailer.lua
@@ -1,3 +1,19 @@
-local file = io.open("/autorun.lua", "w")
-file:write("dofile(\"/OS.lua\")")
-file:close()
+local fs = require("filesystem")
+local component = require("component")
+local computer = require("computer")
+local unicode = require("unicode")
+local shell = require("shell")
+local serialization = require("serialization")
+local gpu = component.gpu
+local screen = component.screen
+
+local function wget(url, path)
+	fs.makeDirectory(fs.path(path))
+	shell.execute("wget " ..url .. " " .. path .. " -fq")
+end
+
+print("123")
+
+--local file = io.open("/autorun.lua", "w")
+--file:write("dofile(\"/OS.lua\")")
+--file:close()
